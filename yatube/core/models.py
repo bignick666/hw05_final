5e8ddc973cee526fe3ebc97b5c1dfba24bcc7614
Nikita Berstnev <bignick666@yandex.ru>
2023-02-25 18:58:53 +0300

    asdasdasd

--- yatube/core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class CreateModel(models.Model):
-    created = models.DateTimeField(verbose_name='Дата создания',
-                                   auto_now_add=True)
-
-    class Meta:
-        abstract = True
