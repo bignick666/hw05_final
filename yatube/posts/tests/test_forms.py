f77e4d32d3d91649c3dd545a36a8e78c4bd9c4ea
Nikita Berstnev <bignick666@yandex.ru>
2023-02-25 19:56:03 +0300

    added test png

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,3 +1,4 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase
 from django.urls import reverse
 
@@ -13,6 +14,7 @@ class PostCreateFormTests(TestCase):
         super().setUpClass()
         cls.user = User.objects.create_user(username='Geek')
 
+
     def setUp(self):
         self.authorized_client = Client()
         self.guest_client = Client()
@@ -20,7 +22,6 @@ class PostCreateFormTests(TestCase):
         self.post = Post.objects.create(
             author=self.user,
             text='Тестовый постик',
-            image='posts/logo.png'
         )
         self.group = Group.objects.create(
             title='Group1',
@@ -31,11 +32,24 @@ class PostCreateFormTests(TestCase):
     def test_post_create(self):
         """Тест формы создания поста"""
         posts_count = Post.objects.count()
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
         form_data = {
             'text': 'Тестовая запись новая',
             'author': self.post.author,
             'group': self.group.pk,
-            'image': self.post.image
+            'image': uploaded
         }
         response = self.authorized_client.post(
             reverse(CREATE_URL),
